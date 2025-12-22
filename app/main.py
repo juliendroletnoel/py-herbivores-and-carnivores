22a0b326cb0edf32809699d269c80c815b3d9c55
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-21 20:03:25 -0500

    First solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,47 @@
-# write your code here
+class Animal (object):
+
+    alive = []
+
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    @classmethod
+    def __str__(cls) -> dict:
+        return {{"Name": animal.name, 
+                 "Health": animal.health, 
+                 "Hidden": animal.hidden} 
+                for animal in Animal.alive}
+
+class Herbivore (Animal):
+
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        super().__init__(name, health, hidden)
+
+    def hide(self) -> None:
+        self.hidden != self.hidden
+
+class Carnivore (Animal):
+
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        super().__init__(name, health, hidden)
+
+    def bite(self, herbivore: Herbivore):
+        if herbivore.hidden:
+            return
+
+        if not isinstance(herbivore, Herbivore):
+            return
+
+        herbivore.health -= 50
